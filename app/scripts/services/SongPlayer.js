664ffd853f7b4395bfb33a1227d8971fa777d310
Joey Li <joeyzaozaoli@gmail.com>
2017-02-12 13:38:57 -0800

    assignment-7 add a private method

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -27,11 +27,20 @@
             currentSong = song;
           };
           
+          /**
+          * @function playSong
+          * @desc Plays song and sets song's playing status to true
+          * @param {Object} song
+          */
+          var playSong = function(song) {
+                currentBuzzObject.play();
+                song.playing = true;
+          };
+          
           SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                     currentBuzzObject.play();
